482f85413e1902962b140f10104e27930950cd09
chocoCui <1017185771@qq.com>
2025-08-07 12:32:20 +0800

    20250807找不到util模块

--- core/web/web_server.py
+++ core/web/web_server.py
@@ -1,4 +1,11 @@
 import os
+import sys
+
+# 添加项目根目录到Python路径
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, project_root)
+# print(project_root,1)
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
